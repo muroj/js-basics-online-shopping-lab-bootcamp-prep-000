c7c198510f4ecdc7f54a4eb5ca243b83d71a5338
Joe Muro <jmuro0321@gmail.com>
2018-07-28 18:55:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -76,7 +76,9 @@ function placeOrder(cardNumber) {
     return `Sorry, we don't have a credit card on file for you.`;
   }
   
+  var total = total();
+  
   cart.splice(0);
   
-  return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
+  return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`;
 }
