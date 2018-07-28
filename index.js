0da614e09e4b283d977985bd1425cc1a1e9e14e4
Joe Muro <jmuro0321@gmail.com>
2018-07-28 18:45:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -61,7 +61,13 @@ function total() {
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for (let i = 0; i < cart.length; i++) {
+    if (cart[i].itemName === item) {
+      return []
+    }
+  }
+  
+  return 'That item is not in your cart.'
 }
 
 function placeOrder(cardNumber) {
