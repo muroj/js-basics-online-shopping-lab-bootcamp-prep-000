31dd0ec9cb0850612bb0eb37f71a065d354a7dfd
Joe Muro <jmuro0321@gmail.com>
2018-07-28 18:49:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ function total() {
 function removeFromCart(item) {
   for (let i = 0; i < cart.length; i++) {
     if (cart[i].itemName === item) {
-      return [cart.splice()]
+      return cart.splice(i, 1)
     }
   }
   
