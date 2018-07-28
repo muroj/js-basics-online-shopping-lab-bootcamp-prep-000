afe272ac349235bbe40ac65f065d93fed3ed5110
Joe Muro <jmuro0321@gmail.com>
2018-07-28 18:33:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,13 +26,16 @@ function viewCart() {
   if (cart.length === 0) {
     return `Your shopping cart is empty.`
   }
+  
   var msg = 'In your cart, you have ';
   
   for (let i = 0; i < cart.length - 1; i++) {
-    msg += `  `
+    msg += `${cart[i].itemName} at ${cart[i].itemPrice}, `
   }
   
+  msg += `and ${cart[cart.length-1].itemName} at ${cart[cart.length-1].itemPrice}.` 
   
+  return msg;
 }
 
 function total() {
