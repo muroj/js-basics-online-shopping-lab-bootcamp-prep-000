2344d74398abe98902a9cd89a70fd6658c6084bd
Joe Muro <jmuro0321@gmail.com>
2018-07-28 18:53:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -76,5 +76,7 @@ function placeOrder(cardNumber) {
     return `Sorry, we don't have a credit card on file for you.`
   }
   
+  cart.
+  
   return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
 }
